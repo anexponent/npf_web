81993a5facc7a418195399945c15a25f692ba876
Azeez Adesina <azeezadesina@yahoo.com>
2025-12-15 16:19:44 +0100

    modified the fpro uploads

--- app/Http/Controllers/Information/fproController.php
+++ app/Http/Controllers/Information/fproController.php
@@ -10,15 +10,42 @@ use Image;
 use Illuminate\Support\Carbon;
 use HTMLPurifier;
 use HTMLPurifier_Config;
-use Log;
 
 class fproController extends Controller
 {
-    public function fproPage () {
+    // Centralized HTMLPurifier instance
+    private function purifier(): HTMLPurifier
+    {
+        $config = HTMLPurifier_Config::createDefault();
+        return new HTMLPurifier($config);
+    }
+
+    // Centralized image saving function
+    private function saveFproImage($file): string
+    {
+        $name = hexdec(uniqid()) . '.' . $file->getClientOriginalExtension();
+        $upload_path = public_path('upload/fpro');
+
+        if (!file_exists($upload_path)) {
+            mkdir($upload_path, 0775, true);
+            chown($upload_path, 'deploy');
+            chgrp($upload_path, 'www-data');
+        }
+
+        $save_path = $upload_path . '/' . $name;
+
+        Image::make($file)->resize(500, 500)->save($save_path);
+
+        return 'upload/fpro/' . $name;
+    }
+
+    public function fproPage()
+    {
         return view('backend.information.fpro_index');
     }
 
-    public function StoreFpro(Request $request){
+    public function StoreFpro(Request $request)
+    {
         // Validation
         $request->validate([
             'fpro_name' => 'required',
@@ -29,19 +56,11 @@ class fproController extends Controller
             'image' => 'required|mimes:jpg,jpeg,png|max:4096',
         ]);
 
-        // Purify HTML
-        $config = HTMLPurifier_Config::createDefault();
-        $purifier = new HTMLPurifier($config);
-        $clean_description = $purifier->purify($request->description);
+        // Purify description
+        $clean_description = $this->purifier()->purify($request->description);
 
-        // Image Upload
-        $image = $request->file('image');
-        $name_gen = hexdec(uniqid()).'.'.$image->getClientOriginalExtension();
-
-        $save_path = public_path('upload/fpro/'.$name_gen);
-        Image::make($image)->resize(500,500)->save($save_path);
-
-        $save_url = 'upload/fpro/'.$name_gen;
+        // Save image
+        $save_url = $this->saveFproImage($request->file('image'));
 
         // Save record
         fpro::insert([
@@ -54,60 +73,53 @@ class fproController extends Controller
             'created_at' => Carbon::now(),
         ]);
 
-        $notification = [
+        return redirect()->route('fpro.index')->with([
             'message' => 'Record Inserted Successfully',
             'alert-type' => 'success'
-        ];
-
-        return redirect()->route('fpro.index')->with($notification);
+        ]);
     }
 
-
-    public function fproDisplay(){
+    public function fproDisplay()
+    {
         $fpro = fpro::latest()->first();
-        return view('frontend.information.fpro_page',compact('fpro'));
+        return view('frontend.information.fpro_page', compact('fpro'));
     }
 
-
-    public function viewFpro() {
-        $fpro = fpro::all(); 
+    public function viewFpro()
+    {
+        $fpro = fpro::all();
         return view('backend.information.fpro_view', compact('fpro'));
     }
 
-
-    public function editFpro($id){
-        $fpro = fpro::find($id);
-        $fpro_id = $id;
-        return view('backend.information.edit_fpro', compact('fpro','fpro_id'));
+    public function editFpro($id)
+    {
+        $fpro = fpro::findOrFail($id);
+        return view('backend.information.edit_fpro', ['fpro' => $fpro, 'fpro_id' => $id]);
     }
 
+    public function fproDelete($id)
+    {
+        $fpro = fpro::findOrFail($id);
 
-    public function fproDelete($id) {
-        $fpro = fpro::find($id);
-
-        // Delete image if exists
+        // Delete old image
         if ($fpro->image && file_exists(public_path($fpro->image))) {
             unlink(public_path($fpro->image));
         }
 
         $fpro->delete();
 
-        $notification = [
-            'message' => 'Contact deleted successfully',
+        return redirect()->route('fpro.view')->with([
+            'message' => 'Record deleted successfully',
             'alert-type' => 'success'
-        ];
-
-        return redirect()->route('fpro.view')->with($notification);  
+        ]);
     }
 
+    public function fproUpdate(Request $request, $id)
+    {
+        $fpro = fpro::findOrFail($id);
 
-    public function fproUpdate(Request $request, $id){
-        $fpro = fpro::find($id);
-
-        // Purify HTML
-        $config = HTMLPurifier_Config::createDefault();
-        $purifier = new HTMLPurifier($config);
-        $clean_description = $purifier->purify($request->description);
+        // Purify description
+        $clean_description = $this->purifier()->purify($request->description);
 
         // Update fields
         $fpro->rank = $request->input('rank');
@@ -116,30 +128,20 @@ class fproController extends Controller
 
         // Image upload if provided
         if ($request->file('image')) {
-
-            // delete old image
+            // Delete old image
             if ($fpro->image && file_exists(public_path($fpro->image))) {
                 unlink(public_path($fpro->image));
             }
 
-            $image = $request->file('image');
-            $name_gen = hexdec(uniqid()).'.'.$image->getClientOriginalExtension();
-
-            $save_path = public_path('upload/fpro/'.$name_gen);
-            Image::make($image)->resize(500,500)->save($save_path);
-
-            $save_url = 'upload/fpro/'.$name_gen;
-            $fpro->image = $save_url;
+            // Save new image
+            $fpro->image = $this->saveFproImage($request->file('image'));
         }
 
         $fpro->save();
 
-        $notification = [
+        return redirect()->route('fpro.view')->with([
             'message' => 'FPRO Profile Updated Successfully',
             'alert-type' => 'success'
-        ];
-
-        return redirect()->route('fpro.view')->with($notification);  
+        ]);
     }
-
 }
